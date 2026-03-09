5aa8c213f15380920cd665b3cb801fef9680857e
Developer <dev@example.com>
2026-03-09 13:43:28 +0700

    fix: move netlify function inside api folder for correct dependency resolution

---
+++ api/netlify/functions/api.js
@@ -0,0 +1,9 @@
+const serverless = require('serverless-http');
+const { app, connectDB } = require('../../server');
+
+const handler = serverless(app);
+
+module.exports.handler = async (event, context) => {
+  await connectDB();
+  return handler(event, context);
+};
